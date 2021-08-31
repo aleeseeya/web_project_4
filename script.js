15f356c723be1ec1e9a04153af9e783e04199d9f
Alicia <aliciamcphoy@gmail.com>
2021-08-31 02:15:15 -0400

    Resolve code review comments

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Variables
+
 let editBtn = document.querySelector(".profile__edit-button");
 let profileName = document.querySelector(".profile__name");
 let profileTitle  = document.querySelector(".profile__title");
@@ -5,9 +7,9 @@ let nameField = document.querySelector("#popup-name");
 let titleField = document.querySelector("#popup-title");
 let popup = document.querySelector(".popup");
 let closeBtn = document.querySelector(".popup__close-button");
+let formElement = document.querySelector(".form");
 
-// Opening and Closing the Popup Box
-// Form Fields
+// Functions
 
 function openPopup() { 
   popup.classList.toggle("popup_opened");
@@ -15,16 +17,10 @@ function openPopup() {
   titleField.value = profileTitle.textContent;
 
 }
-editBtn.addEventListener("click", openPopup);
 
-function closePopup(){
+function closePopup() {
   popup.classList.toggle("popup_opened");
 }
-closeBtn.addEventListener("click", closePopup);
-
-
-// Editing Your Name and About Me
-let formElement = document.querySelector(".form");
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
@@ -33,13 +29,9 @@ function handleFormSubmit(evt) {
   closePopup();
 }
 
+
+// Event Listeners
+
+editBtn.addEventListener("click", openPopup);
+closeBtn.addEventListener("click", closePopup);
 formElement.addEventListener("submit", handleFormSubmit);
-
-
-// Like Icon
-let likeSymbol = document.querySelector(".photo-grid__like-button");
-
-function likePhoto() {
-  likeSymbol.setAttribute("src", "./images/like-icon_active.png");
-}
-likeSymbol.addEventListener("click", likePhoto);
